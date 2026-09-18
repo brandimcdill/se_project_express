273c5069ca384c9cf992aaf65c7483f09aca1989
Brandi McDill <mcdillbrandig@gmail.com>
2026-09-17 22:10:36 -0400

    feat: integrate dotenv environmental configuration architecture

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 import express from "express";
 import mongoose from "mongoose";
 import cors from 'cors';
+import dotenv from 'dotenv';
 import { errors } from 'celebrate';
 import { requestLogger, errorLogger } from './middlewares/logger.js';
 import router from "./routes/index.js";
+
+dotenv.config();
 
 const app = express();
 app.use(cors());
