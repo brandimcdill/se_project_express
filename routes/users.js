d3550a7b401de25063abe0826ff1b550932e64c5
Brandi McDill <mcdillbrandig@gmail.com>
2026-09-23 13:00:44 -0400

    chore: removed unnecessary spaces

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,11 @@
 import express from 'express';
 import { getCurrentUser, updateUser } from "../controllers/users.js";
+import { validateUserBody } from '../middlewares/validation.js';
 
 const usersRouter = express.Router();
 
 usersRouter.get("/me", getCurrentUser);
-
-usersRouter.patch("/me", updateUser);
+usersRouter.patch("/me", validateUserBody, updateUser);
 
 
 export default usersRouter;
