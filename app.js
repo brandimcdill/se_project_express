8b0c69d24a6f40d60a151c51dc3e9fb19d30fe53
Brandi McDill <mcdillbrandig@gmail.com>
2026-09-25 23:25:42 -0400

    fix: remove useless return statement to pass linter tests

--- app.js
+++ app.js
@@ -16,9 +16,7 @@ const { PORT = 3001 } = process.env;
 mongoose
   .connect("mongodb://127.0.0.1:27017/wtwr_db")
   .then(() => {})
-  .catch((err) => {
-    if (err) return;
-  });
+  .catch(() => {});
 
 app.use(express.json());
 
